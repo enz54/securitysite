3905f29f4de95645d55792a19d648c4981c493d5
eric nzenga <enze3968@gmail.com>
2019-09-26 09:57:40 +0200

    UPDATE merged unsaved files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
 const routes = require('./routes')
 app.use(express.static('public'));
 app.get('/favicon.ico', (req, res, next) => {
@@ -13,10 +12,3 @@ app.use('/', routes());
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Our app is running on port ${PORT}`))
-=======
-app
-
-const port = 3000;
-app.listen(port, ()=> console.log(`app is listening at port: ${port}`)
-);
->>>>>>> 4abf87d3bac6b9734edce570e2ca97aa0434c358
